48c70549128dbe263d865dc07e2ca37305620f2a
Tony5300 <tony_tolen@ziggo.nl>
2023-02-15 17:42:20 +0100

    feat: added some box shadown to the border

--- notifications-page-main/src/components/Notifications/Notifications.jsx
+++ notifications-page-main/src/components/Notifications/Notifications.jsx
@@ -7,23 +7,21 @@ const Notifications = () => {
     <main>
       {data.map((data) => {
         return (
-          <div className="notifications-outer-container">
-            <article key={data.id} className="notification-container">
-              <img
-                src={data.image}
-                alt="user-profile"
-                className="notification-img"
-              />
-              <p className="user-name">
-                <strong>{data.name}</strong>
-              </p>
-              <div className="text-container">
-                <p className="followed-you-text">{data.text}</p>
-                <p className="message-box">{data.message}</p>
-                <p>{data.time}</p>
-              </div>
-            </article>
-          </div>
+          <article key={data.id} className="notification-container">
+            <img
+              src={data.image}
+              alt="user-profile"
+              className="notification-img"
+            />
+            <p className="user-name">
+              <strong>{data.name}</strong>
+            </p>
+            <div className="text-container">
+              <p className="followed-you-text">{data.text}</p>
+              <p className="message-box">{data.message}</p>
+              <p>{data.time}</p>
+            </div>
+          </article>
         );
       })}
     </main>
