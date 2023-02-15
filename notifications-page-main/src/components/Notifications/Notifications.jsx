9fadb9d2373e147a2776616cc8b85a784d365678
Tony5300 <tony_tolen@ziggo.nl>
2023-02-15 17:09:17 +0100

    first commit

--- notifications-page-main/src/components/Notifications/Notifications.jsx
+++ notifications-page-main/src/components/Notifications/Notifications.jsx
@@ -7,14 +7,23 @@ const Notifications = () => {
     <main>
       {data.map((data) => {
         return (
-          <article key={data.id}>
-            <img src={data.image} alt="user-profile" />
-            <p>
-              <strong>{data.name}</strong> {data.text}
-            </p>
-            <p>{data.message}</p>
-            <p>{data.time}</p>
-          </article>
+          <div className="notifications-outer-container">
+            <article key={data.id} className="notification-container">
+              <img
+                src={data.image}
+                alt="user-profile"
+                className="notification-img"
+              />
+              <p className="user-name">
+                <strong>{data.name}</strong>
+              </p>
+              <div className="text-container">
+                <p className="followed-you-text">{data.text}</p>
+                <p className="message-box">{data.message}</p>
+                <p>{data.time}</p>
+              </div>
+            </article>
+          </div>
         );
       })}
     </main>
